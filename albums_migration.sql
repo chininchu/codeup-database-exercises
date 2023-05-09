da1a0afa678f006ebeb116c263f68db16ed493e1
chininchu <120990876+chininchu@users.noreply.github.com>
2023-05-09 16:10:10 -0500

    inserted values in table

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist       CHAR(50),
-    artist_name  CHAR(50),
+    record_name  CHAR(50),
     release_date INT,
     sales        FLOAT,
     genre        CHAR(50),
