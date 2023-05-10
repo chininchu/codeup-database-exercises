7e045c4277b163fdf926118d2cba72d63629381c
chininchu <120990876+chininchu@users.noreply.github.com>
2023-05-10 14:07:23 -0500

    Basic Statements Final Commit

--- update_exercises.sql
+++ update_exercises.sql
@@ -8,4 +8,58 @@ SELECT * FROM albums;
 
 SELECT * FROM albums WHERE release_date < 1980;
 
+-- Albums by Micheal Jackson
+
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+
+-- Update all albums 10 times more popular
+
+UPDATE albums SET sales = sales * 10;
+
+-- Verify that sale records have been updated
+
+SELECT * FROM albums;
+
+-- Moving albums before 1980 to 1800s
+
+SELECT * FROM albums WHERE release_date < 1980;
+
+UPDATE albums SET release_date = 1800 WHERE release_date < 1980;
+
+SELECT * FROM albums;
+
+-- Change the name of the artist from Michael Jackson to Peter Jackson
+
+SELECT * FROM albums WHERE artist = 'Michael Jackson';
+
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
+
+SELECT * FROM albums WHERE artist = 'Peter Jackson';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
